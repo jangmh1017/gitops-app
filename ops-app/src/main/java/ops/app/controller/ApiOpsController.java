54fd12ac38149c3a12d01be37d1b5eb238641701
jangmh1017 <jangmh697@gmail.com>
2026-05-18 15:14:11 +0900

    feat: 초기 앱 구성

---
+++ ops-app/src/main/java/ops/app/controller/ApiOpsController.java
@@ -0,0 +1,22 @@
+package ops.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController("api")
+public class ApiOpsController {
+
+	@GetMapping("/ops")
+	public String OpsTest() {
+		return "Get - /api/ops";
+	}
+	
+	@GetMapping("health")
+	public String healthCheck() {
+		return "OK";
+	}
+	
+}
