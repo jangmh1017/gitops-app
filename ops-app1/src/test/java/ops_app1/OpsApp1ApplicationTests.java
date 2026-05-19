f7acc771cbcb258267bca92b327967c6148fda3b
jangmh1017 <jangmh697@gmail.com>
2026-05-19 13:41:31 +0900

    update mistake

--- ops-app1/src/test/java/ops_app1/OpsApp1ApplicationTests.java
+++ ops-app1/src/test/java/ops_app1/OpsApp1ApplicationTests.java
@@ -1,4 +1,4 @@
-package ops_app1;
+package ops-app1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- ops-app2/src/test/java/ops_app2/OpsApp2ApplicationTests.java
+++ ops-app2/src/test/java/ops_app2/OpsApp2ApplicationTests.java
@@ -1,4 +1,4 @@
-package ops_app2;
+package ops.app2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
